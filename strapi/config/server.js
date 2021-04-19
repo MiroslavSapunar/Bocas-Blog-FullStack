d63397e807238843383e18ec7f8066587c387814
Miroslav <psapunar@fi.uba.ar>
2021-04-19 01:57:13 -0300

    docker

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a2a7245d9b042b4633fab78145c29c10'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a3fdf2b1-838a-4f92-9923-b505f55d603a'
+};
